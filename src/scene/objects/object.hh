31dc23740b05177e23bcacb1fcce99e67dd30d04
Dominique MICHEL <dominique.michel@epita.fr>
2021-04-01 18:45:51 +0200

    Remove old object raytracing code

--- src/scene/objects/object.hh
+++ src/scene/objects/object.hh
@@ -13,13 +13,14 @@ public:
         : material_(material)
     {}
 
-    virtual std::vector<Point3<>> getIntersect(const Ray& ray) = 0; // FIXME / TODO: define rayon
-
     virtual double getSignedDistance(const Point3<>& fromPoint) = 0;
 
-    virtual Vector3<> getNormale(const Point3<>& pos) = 0;
-    virtual Texture getTexture(const Point3<>& pos) = 0;
-    virtual ColorRGB getColor(const Point3<>& pos) = 0;
+    virtual Texture getTexture(const Point3<>& pos)
+    { return material_->getTexture(pos); }
+
+    virtual ColorRGB getColor(const Point3<>& pos)
+    { return material_->getColor(pos); }
+
 
 protected:
     std::shared_ptr<TextureMaterial> material_;
